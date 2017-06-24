64824ecfe1c9316fb0a7e62051aff50820bbffdf
Gustavo <jg_barreto@hotmail.com>
2017-06-24 19:51:21 -0400

    added developer env

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const path = require('path');
+/*const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -33,3 +33,4 @@ app.get('/*', function (req, res) {
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+*/
